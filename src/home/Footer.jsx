b7c4404a79ae5670889a63dd72f37fc654447848
jen <jayhunt824@gmail.com>
2021-02-25 16:33:06 -0500

    combined style code

--- src/home/Footer.jsx
+++ src/home/Footer.jsx
@@ -6,9 +6,16 @@ const Footer = () => {
     const theDate = new Date().getFullYear();
 
     return (
+        
+
+       
+        <div className="footer">
+            
         <div className='footer-copyright text-center ml-auto py-3'>  
             &copy; {theDate} - Inception Team - Blue Badge      
         </div>
+        </div>
+      
     );
 };
 
