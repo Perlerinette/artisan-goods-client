ec7ed8b4fcb9577d59de0d1f04411d7adec9c331
Perlerinette <laurine.reyre@gmail.com>
2021-03-02 10:05:09 -0500

    added deleteImg, default image

--- src/products/HomeGalleryChild.jsx
+++ src/products/HomeGalleryChild.jsx
@@ -16,77 +16,48 @@ const HomeGalleryChild = (props) => {
     className
   } = props;
 
+  // to display the close button on Modal form
   const [modal, setModal] = useState(false);
-
   const toggle = () => setModal(!modal);
+
+  //used in case no picture set in the card 
+  function errorHandling(ev){
+    ev.target.src = 'https://askleo.askleomedia.com/wp-content/uploads/2004/06/no_image-300x245.jpg';
+  }
 
     return ( 
      
       <div className="Cards">
       <Card className="CardCss" style={{textAlign: "center" , width: '350px', height: "450px", padding: "0px", marginBottom: 20, boxShadow: " lightGrey 2px 2px"}}>
-        <CardImg  src={props.productItem.photoURL}  width="350" height="270"  alt="Card image" />
-
+        <CardImg src={props.productItem.photoURL} alt="Card image" width="350" height="270" onError = {errorHandling} />
         <CardBody className="CardBody" style = {{background: "white"}} >
           <CardTitle tag="h5" style={{fontSize: 17}}>{props.productItem.name}</CardTitle>
           <CardSubtitle tag="h6" className="mb-2 text-muted">${props.productItem.price}</CardSubtitle>
           <CardText>
             {props.productItem.availability === true ? <p className='text-success'>In stock</p> : <p className='text-danger'>Out of stock</p>}
-            </CardText>
-            <Button style={{background: "#4A5759"}} onClick={toggle}>{buttonLabel}See Full Description</Button> 
+          </CardText>
+          <Button style={{background: "#4A5759"}} onClick={toggle}>{buttonLabel}See Full Description</Button> 
         </CardBody>
       </Card>
+
       <Modal isOpen={modal} toggle={toggle} className={className}>
-        <ModalHeader style={{backgroundColor: "#f7e1d7"}} toggle={toggle}>{props.productItem.name}</ModalHeader>
-        
+        <ModalHeader style={{backgroundColor: "#f7e1d7" , textAlign: "center"}} toggle={toggle}>
+          {props.productItem.name}
+          {props.productItem.availability === true ? <p className='text-success'>In stock</p> : <p className='text-danger'>Out of stock</p>}
+        </ModalHeader>
         <ModalBody style={{textAlign: "center"}}>
-          <img style={{ width:350, height:270}}src={props.productItem.photoURL}></img>
-          <br/>
-          <br/>
-          <div>
-      
-        {props.productItem.description}
-        <br/>
-        {props.productItem.availability === true ? <p className='text-success'>In stock</p> : <p className='text-danger'>Out of stock</p>}
-        ${props.productItem.price}
-        </div>
+          <CardImg src={props.productItem.photoURL} alt="Card image" width="350" height="270" onError = {errorHandling} />
+          <CardSubtitle tag="h6" className="mb-2 text-muted">${props.productItem.price}</CardSubtitle>
+          <CardText>
+            {props.productItem.description}{' '}
+          </CardText>
         </ModalBody>
         <ModalFooter style={{backgroundColor: "#f7e1d7"}}>
           <Button style={{backgroundColor: "#4a5759", color: "white", width: 150, height: 40, textAlign: "center", marginRight: "auto", marginLeft: "auto"}}  color="primary" onClick={toggle}>Close</Button>{' '}
-         
         </ModalFooter>
       </Modal>
-     
-   
-       
-        {/* <Modal style={{width: 600}}
-        isOpen={isOpen}
-        onRequestClose={toggleModal}
-        contentLabel="My dialog"
-    
-
         
-         
-        >
-        <div>
-        <Card  className="cardModal" style={{width: 500}}>
-        <CardBody style={{textAlign: 'center'}}>
-          <CardImg src={props.productItem.photoURL} alt="card image" style={{width:   '450px', height: '300px', padding: "20px"  }}  /> 
-          <CardTitle tag="h2">{props.productItem.name}</CardTitle>
-          <CardSubtitle tag="h5" className="mb-2 text-muted">${props.productItem.price}</CardSubtitle>
-          <CardText tag="h4">Description:</CardText>
-          <CardText tag="h6">{props.productItem.description}</CardText>
-          <CardText>
-            {props.productItem.availability === true ? <p className='text-success'>In stock</p> : <p className='text-danger'>Out of stock</p>}
-            </CardText>
-        </CardBody>
-      </Card>
-        </div>
-        <div className='align-middle text-center'>
-        <button style={{backgroundColor: "#4a5759", color: "white", width: 150, height: 40}} onClick={toggleModal}>Close</button>
-        </div>
-        </Modal> */}
-        </div>
-      
+      </div>
         
       );
     };
--- src/products/MyPageCards.jsx
+++ src/products/MyPageCards.jsx
@@ -7,14 +7,18 @@ import { Card, Button, CardImg, CardTitle, CardText, CardSubtitle, CardBody, Row
 
 
 
-const HomeGalleryChild = (props) => {
+const MyPageCards = (props) => {
 
+  //used in case no picture set in the card 
+  function errorHandling(ev){
+    ev.target.src =  'https://askleo.askleomedia.com/wp-content/uploads/2004/06/no_image-300x245.jpg';
+  }
 
-    return ( 
-      <div>
-        <Card className="myShopCard" style={{width: 550,  height: 450, marginLeft: "16%", backgroundColor: "#f5f5f5"}}>
+  return ( 
+    <div>
+      <Card className="myShopCard" style={{width: 550,  height: 450, marginLeft: "16%", backgroundColor: "#f5f5f5"}}>
         <CardBody style={{}}>
-          <CardImg src={props.productItem.photoURL} alt="card image" style={{width:   '175px'  }} /> 
+          <CardImg src={props.productItem.photoURL} alt="card image" style={{width:   '175px'  }} onError = {errorHandling} /> 
           <CardTitle tag="h5">{props.productItem.name}</CardTitle>
           <CardSubtitle tag="h6" className="mb-2 text-muted">${props.productItem.price}</CardSubtitle>
           <CardText>
@@ -23,16 +27,11 @@ const HomeGalleryChild = (props) => {
           <div style={{ borderRadius: 5, padding: 20, display: "block"}}>
           <CardText tag="h6">{props.productItem.description}</CardText>
           </div>
-         
-            
-            
-            
-            
         </CardBody>
       </Card>
 
-        </div>
-      );
-    };
+      </div>
+    );
+  };
  
-export default HomeGalleryChild;
+export default MyPageCards;
